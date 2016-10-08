d51e82bbff8d877e19b023677d101243c57a7262
FaustYKKN <lingyingcome@163.com>
2016-10-08 19:34:51 +0800

    placeholder problem fixed

--- js/topfoot.js
+++ js/topfoot.js
@@ -1,4 +1,65 @@
 $(function(){
-	$('.top').load('top.html');
+	$('.top').load('top.html',function(){
+		var pulldown = {
+			init: function(){
+				//定义变量对象
+				this.searchType = $('.SK-search-type');
+				this.triangle = $('.SK-search-type-triangle');
+				this.typeUl = $('.SK-search-type ul');
+				this.owner = $('.owner');
+				this.ownerUl = $('.SK-pulldown-menu');
+				//调用方法
+				this.enter();
+				this.leave();
+			},
+			enter: function(){
+				var that = this;
+				this.searchType.mouseenter(function(){
+					that.showSearch();
+				});
+				this.owner.mouseenter(function(){
+					that.showOwnerUl();
+				});
+			},
+			leave: function(){
+				var that = this;
+				this.searchType.mouseleave(function(){
+					that.hideSearch();
+				});
+				this.owner.mouseleave(function(){
+					that.hideOwnerUl();
+				});
+			},
+			showSearch: function(){
+				this.triangle.css({
+					borderColor: '#2b2b2b transparent transparent transparent',
+					top: 13,
+					right: 4
+				});
+				this.typeUl.removeClass('SK-search-type-hide');
+				this.typeUl.addClass('SK-search-type-ul');
+			},
+			hideSearch: function(){
+				this.triangle.css({
+					borderColor: 'transparent transparent transparent #2b2b2b',
+					top: 11,
+					right: 2
+				});
+				this.typeUl.removeClass('SK-search-type-ul');
+				this.typeUl.addClass('SK-search-type-hide');
+			},
+			showOwnerUl: function(){
+				this.ownerUl.css({
+					display: 'block'
+				});
+			},
+			hideOwnerUl: function(){
+				this.ownerUl.css({
+					display: 'none'
+				});
+			},
+		};
+		pulldown.init();
+	});
 	$('.foot').load('footer.html');
 });
